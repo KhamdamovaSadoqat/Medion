90f7235c7371769a9532440a060f8d844fa96e9d
DARKMATTER <sadoqathamdamova2202@gmail.com>
2021-10-05 18:43:50 +0500

    pushing befor migrating to androidX

--- app/src/main/java/uz/medion/ui/main/user/ourDoctors/OurDoctorsFragment.kt
+++ app/src/main/java/uz/medion/ui/main/user/ourDoctors/OurDoctorsFragment.kt
@@ -1,15 +1,19 @@
 package uz.medion.ui.main.user.ourDoctors
 
-import android.view.View
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
-import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.flexbox.FlexDirection
+import com.google.android.flexbox.FlexboxLayoutManager
+import com.google.android.flexbox.JustifyContent
 import uz.medion.R
 import uz.medion.data.constants.Constants
 import uz.medion.databinding.FragmentOurDoctorsBinding
 import uz.medion.ui.base.BaseFragment
+import uz.medion.utils.gone
+import uz.medion.utils.visible
+
 
 class OurDoctorsFragment : BaseFragment<FragmentOurDoctorsBinding, OurDoctorsVM>() {
 
@@ -31,13 +35,16 @@ class OurDoctorsFragment : BaseFragment<FragmentOurDoctorsBinding, OurDoctorsVM>
             if (tvCategoryAll) {
                 binding.rvDoctorsCategories.layoutManager =
                     LinearLayoutManager(requireContext(), RecyclerView.HORIZONTAL, false)
-                binding.clReason.visibility = View.GONE
-                binding.clCalendar.visibility = View.GONE
+                binding.clReason.gone()
+                binding.clCalendar.gone()
                 tvCategoryAll = false
             } else {
-                binding.rvDoctorsCategories.layoutManager = GridLayoutManager(requireContext(), 3)
-                binding.clReason.visibility = View.VISIBLE
-                binding.clCalendar.visibility = View.VISIBLE
+                val flexboxLayoutManager = FlexboxLayoutManager(context)
+                flexboxLayoutManager.flexDirection = FlexDirection.COLUMN
+                flexboxLayoutManager.justifyContent = JustifyContent.FLEX_END
+                binding.rvDoctorsCategories.layoutManager = flexboxLayoutManager
+                binding.clReason.visible()
+                binding.clCalendar.visible()
                 tvCategoryAll = true
             }
         }
@@ -51,8 +58,22 @@ class OurDoctorsFragment : BaseFragment<FragmentOurDoctorsBinding, OurDoctorsVM>
             LinearLayoutManager(requireContext(), RecyclerView.VERTICAL, false)
 
         binding.cvCalendar.date = System.currentTimeMillis()
-//        binding.cvCalendar.
 
+        binding.clOption1.setOnClickListener {
+            binding.ivOption1.visible()
+            binding.ivOption2.gone()
+            binding.ivOption3.gone()
+        }
+        binding.clOption2.setOnClickListener {
+            binding.ivOption1.gone()
+            binding.ivOption2.visible()
+            binding.ivOption3.gone()
+        }
+        binding.clOption3.setOnClickListener {
+            binding.ivOption1.gone()
+            binding.ivOption2.gone()
+            binding.ivOption3.visible()
+        }
 
     }
 
