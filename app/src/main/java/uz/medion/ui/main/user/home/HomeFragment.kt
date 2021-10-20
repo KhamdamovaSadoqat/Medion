4f33d5d04d059bff911970f2936d02e8ecfda119
DARKMATTER <sadoqathamdamova2202@gmail.com>
2021-10-20 11:15:47 +0500

    GridLayout updated in home page

--- app/src/main/java/uz/medion/ui/main/user/home/HomeFragment.kt
+++ app/src/main/java/uz/medion/ui/main/user/home/HomeFragment.kt
@@ -2,19 +2,22 @@ package uz.medion.ui.main.user.home
 
 import android.view.Gravity
 import android.widget.GridLayout
-import android.widget.RelativeLayout
 import androidx.annotation.LayoutRes
-import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.view.marginStart
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.flexbox.FlexDirection
+import com.google.android.flexbox.FlexboxLayoutManager
+import com.google.android.flexbox.JustifyContent
 import uz.medion.R
 import uz.medion.data.constants.Constants
 import uz.medion.databinding.FragmentHomeBinding
 import uz.medion.ui.base.BaseFragment
 import uz.medion.ui.main.MainActivity
+import uz.medion.utils.ViewUtils.setMargins
 
 
 class HomeFragment : BaseFragment<FragmentHomeBinding, HomeVM>() {
@@ -26,33 +29,34 @@ class HomeFragment : BaseFragment<FragmentHomeBinding, HomeVM>() {
         setUp()
     }
 
-    fun setUp(){
+    fun setUp() {
         adapter = HomeAdapter {
-            when(it){
+            when (it) {
                 it -> findNavController().navigate(R.id.ourDoctorsFragment)
             }
         }
         adapter.setData(Constants.getHomeItems())
         binding.rvCategories.adapter = adapter
-        binding.rvCategories.layoutManager = LinearLayoutManager(requireContext(), RecyclerView.HORIZONTAL, false)
+        binding.rvCategories.layoutManager =
+            LinearLayoutManager(requireContext(), RecyclerView.HORIZONTAL, false)
 
         binding.tvAll.setOnClickListener {
-            if(tvAll){
-                binding.rvCategories.layoutManager = LinearLayoutManager(requireContext(), RecyclerView.HORIZONTAL, false)
+            if (tvAll) {
+                binding.rvCategories.layoutManager =
+                    LinearLayoutManager(requireContext(), RecyclerView.HORIZONTAL, false)
                 tvAll = false
-            }else{
+            } else {
                 binding.rvCategories.layoutManager = GridLayoutManager(requireContext(), 3)
-//                val overviewParams = GridLayout.LayoutParams()
-//                overviewParams.setGravity(Gravity.CENTER_HORIZONTAL)
-//                binding.rvCategories.layoutParams = overviewParams
+                setMargins(binding.rvCategories, 50, 0, 0, 0)
                 tvAll = true
+
             }
         }
     }
 
-    fun setUpUI(){
-        val activity = requireActivity() as MainActivity
-    }
+//    fun setUpUI(){
+//        val activity = requireActivity() as MainActivity
+//    }
 
     @LayoutRes
     override fun getLayoutResId(): Int = R.layout.fragment_home
