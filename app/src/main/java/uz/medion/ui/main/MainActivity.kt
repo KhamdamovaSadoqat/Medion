0b8a86bfe29ae06af4266b7cf6762851d330640c
SadoqatKhamdamova <sadoqathamdamova2202@gmail.com>
2021-11-11 01:55:04 +0500

    Major changes in MainActivity.kt

--- app/src/main/java/uz/medion/ui/main/MainActivity.kt
+++ app/src/main/java/uz/medion/ui/main/MainActivity.kt
@@ -35,185 +35,74 @@ class MainActivity : BaseActivity<ActivityMainBinding, MainVM>() {
         bottomNavController =
             BottomNavController(binding, binding.partialBottomNav, this, navController)
         navController.addOnDestinationChangedListener { _, destination, _ ->
-            //from where bottom navigation should be removed
-            if (destination.id == R.id.aboutDoctorFragment || destination.id == R.id.verificationFragment2) {
-                ViewUtils.fadeOut(binding.partialBottomNav.root)
-            } else {
-                ViewUtils.fadeIn(binding.partialBottomNav.root)
-            }
-            //here goes HomeFragment
+
+            //where bottom navigation should be removed
+            if (destination.id == R.id.aboutDoctorFragment ||
+                destination.id == R.id.verificationFragment2 ||
+                destination.id == R.id.changeNumberFragment ||
+                destination.id == R.id.changePasswordFragment ||
+                destination.id == R.id.chooseLanguageFragment ||
+                destination.id == R.id.myDocumentsFragment ||
+                destination.id == R.id.personalDateFragment ||
+                destination.id == R.id.esteticMedicineFragment
+            ) ViewUtils.fadeOut(binding.partialBottomNav.root)
+            else ViewUtils.fadeIn(binding.partialBottomNav.root)
+
+            //where arrow_back and text should be removed medion logo should be displayed
             if (destination.id == R.id.homeFragment) {
-                ViewUtils.fadeIn(binding.ivMedion)
-                ViewUtils.fadeIn(binding.ivNotification)
+                ViewUtils.fadeOut(binding.tvMain)
                 ViewUtils.fadeOut(binding.ivBackArrow)
-                ViewUtils.fadeOut(binding.tvMain)
-                ViewUtils.fadeOut(binding.ivSearch)
-                ViewUtils.fadeOut(binding.ivHeart)
-                binding.partialBottomNav.bottomNavigationViewHome.visible()
-            }
-            //here goes OurDoctorsFragment
-            if (destination.id == R.id.ourDoctorsFragment) {
-                binding.tvMain.setText(R.string.our_doctors)
+                ViewUtils.fadeIn(binding.ivMedion)
+            } else {
                 ViewUtils.fadeIn(binding.ivBackArrow)
                 ViewUtils.fadeIn(binding.tvMain)
-                ViewUtils.fadeIn(binding.ivSearch)
-                ViewUtils.fadeOut(binding.ivNotification)
-                ViewUtils.fadeOut(binding.ivHeart)
-                ViewUtils.fadeOut(binding.ivMedion)
-                binding.partialBottomNav.bottomNavigationViewHome.visible()
-            }
-            //here goes AboutDoctorsFragment
-            if (destination.id == R.id.aboutDoctorFragment) {
-                binding.tvMain.setText(R.string.our_doctors)
-                ViewUtils.fadeIn(binding.ivBackArrow)
-                ViewUtils.fadeIn(binding.ivHeart)
-                ViewUtils.fadeOut(binding.tvMain)
-                ViewUtils.fadeOut(binding.ivMedion)
-                ViewUtils.fadeOut(binding.ivSearch)
-                ViewUtils.fadeOut(binding.ivNotification)
-                binding.partialBottomNav.bottomNavigationViewHome.visible()
-            }
-            //here goes CertificateFragment
-            if (destination.id == R.id.certificateFragment) {
-                binding.tvMain.setText(R.string.image)
-                ViewUtils.fadeIn(binding.ivBackArrow)
-                ViewUtils.fadeIn(binding.tvMain)
-                ViewUtils.fadeOut(binding.ivHeart)
-                ViewUtils.fadeOut(binding.ivMedion)
-                ViewUtils.fadeOut(binding.ivSearch)
-                ViewUtils.fadeOut(binding.ivNotification)
-                binding.partialBottomNav.bottomNavigationViewHome.visible()
-            }
-            //here goes PersonalAccountFragment
-            if (destination.id == R.id.personalAccountFragment) {
-                binding.tvMain.setText(R.string.personal_room)
-                ViewUtils.fadeIn(binding.ivBackArrow)
-                ViewUtils.fadeIn(binding.ivNotification)
-                ViewUtils.fadeIn(binding.tvMain)
-                ViewUtils.fadeOut(binding.ivHeart)
-                ViewUtils.fadeOut(binding.ivMedion)
-                ViewUtils.fadeOut(binding.ivSearch)
-                binding.partialBottomNav.bottomNavigationViewHome.visible()
-            }
-            //here goes ChangeNumberFragment
-            if (destination.id == R.id.changeNumberFragment) {
-                binding.tvMain.setText(R.string.change_mobile_phone_number)
-                ViewUtils.fadeIn(binding.ivBackArrow)
-                ViewUtils.fadeIn(binding.tvMain)
-                ViewUtils.fadeOut(binding.ivNotification)
-                ViewUtils.fadeOut(binding.ivHeart)
-                ViewUtils.fadeOut(binding.ivMedion)
-                ViewUtils.fadeOut(binding.ivSearch)
-                binding.partialBottomNav.bottomNavigationViewHome.gone()
-            }
-            //here goes ChangePasswordFragment
-            if (destination.id == R.id.changePasswordFragment) {
-                binding.tvMain.setText(R.string.change_password)
-                ViewUtils.fadeIn(binding.ivBackArrow)
-                ViewUtils.fadeIn(binding.tvMain)
-                ViewUtils.fadeOut(binding.ivNotification)
-                ViewUtils.fadeOut(binding.ivHeart)
-                ViewUtils.fadeOut(binding.ivMedion)
-                ViewUtils.fadeOut(binding.ivSearch)
-                binding.partialBottomNav.bottomNavigationViewHome.gone()
-            }
-            //here goes ChooseLanguageFragment
-            if (destination.id == R.id.chooseLanguageFragment) {
-                binding.tvMain.setText(R.string.choose_language)
-                ViewUtils.fadeIn(binding.ivBackArrow)
-                ViewUtils.fadeIn(binding.tvMain)
-                ViewUtils.fadeOut(binding.ivNotification)
-                ViewUtils.fadeOut(binding.ivHeart)
-                ViewUtils.fadeOut(binding.ivMedion)
-                ViewUtils.fadeOut(binding.ivSearch)
-                binding.partialBottomNav.bottomNavigationViewHome.gone()
-            }
-            //here goes PersonalDataFragment
-            if (destination.id == R.id.personalDateFragment) {
-                binding.tvMain.setText(R.string.personal_information)
-                ViewUtils.fadeIn(binding.ivBackArrow)
-                ViewUtils.fadeIn(binding.tvMain)
-                ViewUtils.fadeOut(binding.ivNotification)
-                ViewUtils.fadeOut(binding.ivHeart)
-                ViewUtils.fadeOut(binding.ivMedion)
-                ViewUtils.fadeOut(binding.ivSearch)
-                binding.partialBottomNav.bottomNavigationViewHome.gone()
-            }
-            //here goes MyDoctorsFragment
-            if (destination.id == R.id.myDoctorsFragment) {
-                binding.tvMain.setText(R.string.my_doctors)
-                ViewUtils.fadeIn(binding.ivBackArrow)
-                ViewUtils.fadeIn(binding.tvMain)
-                ViewUtils.fadeIn(binding.ivSearch)
-                ViewUtils.fadeOut(binding.ivNotification)
-                ViewUtils.fadeOut(binding.ivHeart)
-                ViewUtils.fadeOut(binding.ivMedion)
-            }
-            //here goes MyDocumentsFragment
-            if (destination.id == R.id.myDocumentsFragment) {
-                binding.tvMain.setText(R.string.my_documents)
-                ViewUtils.fadeIn(binding.ivBackArrow)
-                ViewUtils.fadeIn(binding.tvMain)
-                ViewUtils.fadeIn(binding.ivSearch)
-                ViewUtils.fadeOut(binding.ivNotification)
-                ViewUtils.fadeOut(binding.ivHeart)
-                ViewUtils.fadeOut(binding.ivMedion)
-                binding.partialBottomNav.bottomNavigationViewHome.gone()
-            }
-            //here goes AddressAndContactsFragment
-            if (destination.id == R.id.myDocumentsFragment) {
-                binding.tvMain.setText(R.string.adress_contact)
-                ViewUtils.fadeIn(binding.ivBackArrow)
-                ViewUtils.fadeIn(binding.tvMain)
-                ViewUtils.fadeIn(binding.ivSearch)
-                ViewUtils.fadeOut(binding.ivNotification)
-                ViewUtils.fadeOut(binding.ivHeart)
-                ViewUtils.fadeOut(binding.ivMedion)
-            }
-            //here goes AddressFragment
-            if (destination.id == R.id.myDocumentsFragment) {
-                binding.tvMain.setText(R.string.adress)
-                ViewUtils.fadeIn(binding.ivBackArrow)
-                ViewUtils.fadeIn(binding.tvMain)
-                ViewUtils.fadeIn(binding.ivSearch)
-                ViewUtils.fadeOut(binding.ivNotification)
-                ViewUtils.fadeOut(binding.ivHeart)
-                ViewUtils.fadeOut(binding.ivMedion)
-                binding.partialBottomNav.bottomNavigationViewHome.gone()
-            }
-            //here goes EstethicMedicineFragment
-            if (destination.id == R.id.esteticMedicineFragment) {
-                binding.tvMain.setText(R.string.estethic_medicine)
-                ViewUtils.fadeIn(binding.ivBackArrow)
-                ViewUtils.fadeIn(binding.tvMain)
-                ViewUtils.fadeIn(binding.ivNotification)
-                ViewUtils.fadeOut(binding.ivSearch)
-                ViewUtils.fadeOut(binding.ivHeart)
-                ViewUtils.fadeOut(binding.ivMedion)
-                binding.partialBottomNav.bottomNavigationViewHome.gone()
-            }
-            //here goes SpaMedicineFragment
-            if (destination.id == R.id.esteticMedicineFragment) {
-                binding.tvMain.setText(R.string.spa_medicine)
-                ViewUtils.fadeIn(binding.ivBackArrow)
-                ViewUtils.fadeIn(binding.tvMain)
-                ViewUtils.fadeIn(binding.ivNotification)
-                ViewUtils.fadeOut(binding.ivSearch)
-                ViewUtils.fadeOut(binding.ivHeart)
-                ViewUtils.fadeOut(binding.ivMedion)
-            }
-            //here goes AppointmentEnrollFragment, AppointmentFragment
-            if (destination.id == R.id.appointmentEnrollFragment || destination.id == R.id.appointmentFragment) {
-                binding.tvMain.setText(R.string.constultation)
-                ViewUtils.fadeIn(binding.ivBackArrow)
-                ViewUtils.fadeIn(binding.tvMain)
-                ViewUtils.fadeIn(binding.ivNotification)
-                ViewUtils.fadeOut(binding.ivSearch)
-                ViewUtils.fadeOut(binding.ivHeart)
                 ViewUtils.fadeOut(binding.ivMedion)
             }
 
+            //where search should be displayed
+            if (destination.id == R.id.ourDoctorsFragment ||
+                destination.id == R.id.myDoctorsFragment ||
+                destination.id == R.id.myDocumentsFragment ||
+                destination.id == R.id.addressFragment ||
+                destination.id == R.id.adressAndContactsFragment
+            ) ViewUtils.fadeIn(binding.ivSearch)
+            else ViewUtils.fadeOut(binding.ivSearch)
+
+            //where notification should be displayed
+            if (destination.id == R.id.homeFragment ||
+                destination.id == R.id.personalAccountFragment ||
+                destination.id == R.id.esteticMedicineFragment ||
+                destination.id == R.id.spaMedicineFragment ||
+                destination.id == R.id.appointmentEnrollFragment ||
+                destination.id == R.id.appointmentFragment
+            ) ViewUtils.fadeIn(binding.ivNotification)
+            else ViewUtils.fadeOut(binding.ivNotification)
+
+            //where heart should be displayed
+            if (destination.id == R.id.aboutDoctorFragment) ViewUtils.fadeIn(binding.ivHeart)
+            else ViewUtils.fadeOut(binding.ivHeart)
+
+            when (destination.id) {
+                R.id.adressAndContactsFragment -> binding.tvMain.setText(R.string.adress_contact)
+                R.id.appointmentEnrollFragment -> binding.tvMain.setText(R.string.constultation)
+                R.id.esteticMedicineFragment -> binding.tvMain.setText(R.string.estethic_medicine)
+                R.id.personalAccountFragment -> binding.tvMain.setText(R.string.personal_room)
+                R.id.changePasswordFragment -> binding.tvMain.setText(R.string.change_password)
+                R.id.chooseLanguageFragment -> binding.tvMain.setText(R.string.choose_language)
+                R.id.changeNumberFragment -> binding.tvMain.setText(R.string.change_mobile_phone_number)
+                R.id.personalDateFragment -> binding.tvMain.setText(R.string.personal_information)
+                R.id.appointmentFragment -> binding.tvMain.setText(R.string.constultation)
+                R.id.myDocumentsFragment -> binding.tvMain.setText(R.string.my_documents)
+                R.id.spaMedicineFragment -> binding.tvMain.setText(R.string.spa_medicine)
+                R.id.aboutDoctorFragment -> binding.tvMain.setText(R.string.about_doctor)
+                R.id.certificateFragment -> binding.tvMain.setText(R.string.image)
+                R.id.ourDoctorsFragment -> binding.tvMain.setText(R.string.our_doctors)
+                R.id.myDoctorsFragment -> binding.tvMain.setText(R.string.my_doctors)
+                R.id.addressFragment -> binding.tvMain.setText(R.string.adress)
+                R.id.addCardFragment -> binding.tvMain.setText(R.string.add_card)
+            }
         }
+
         binding.ivBackArrow.setOnClickListener { onBackPressed() }
 
         binding.nvNavigationDrawer.setNavigationItemSelectedListener { menuItem ->
@@ -228,18 +117,13 @@ class MainActivity : BaseActivity<ActivityMainBinding, MainVM>() {
             binding.dlMenu.closeDrawer(GravityCompat.START)
             true
         }
-
-
     }
-
 
     @LayoutRes
     override fun getLayoutResId() = R.layout.activity_main
     override val vm: MainVM by viewModels()
 
-    override fun setStatusBarBackgroundHeight(statusBarBackground: View) {
-
-    }
+    override fun setStatusBarBackgroundHeight(statusBarBackground: View) {}
 
     override fun onSupportNavigateUp(): Boolean {
         return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
@@ -248,36 +132,15 @@ class MainActivity : BaseActivity<ActivityMainBinding, MainVM>() {
     override fun onBackPressed() {
 //        val navigationOptions =
 //            NavOptions.Builder().setPopUpTo(navController.currentDestination!!.id, true).build()
-        if (navController.currentDestination!!.id == R.id.changeNumberFragment ||
-            navController.currentDestination!!.id == R.id.changePasswordFragment ||
-            navController.currentDestination!!.id == R.id.chooseLanguageFragment ||
-            navController.currentDestination!!.id == R.id.personalDateFragment ||
-            navController.currentDestination!!.id == R.id.myDoctorsFragment ||
-            navController.currentDestination!!.id == R.id.myDocumentsFragment
-        ) {
-            navController.popBackStack(R.id.personalAccountFragment, false)
-            // navController.navigate(R.id.olympiadFragment, null, navigationOptions)
-        } else if (navController.currentDestination!!.id == R.id.addressFragment) {
-            navController.popBackStack(R.id.adressAndContactsFragment, false)
-        } else if (navController.currentDestination!!.id == R.id.spaMedicineDetailsFragment) {
-            navController.popBackStack(R.id.spaMedicineFragment, false)
-        } else if (navController.currentDestination!!.id == R.id.appointmentFragment ||
-            navController.currentDestination!!.id == R.id.appointmentEnrollFragment ||
-            navController.currentDestination!!.id == R.id.paymentCompleteFragment
+//        navController.navigate(R.id.olympiadFragment, null, navigationOptions)
+        if (navController.currentDestination!!.id == R.id.paymentCompleteFragment
         ) { navController.popBackStack(R.id.aboutDoctorFragment, false)
-        } else if (navController.currentDestination!!.id == R.id.addCardFragment) {
-            navController.popBackStack(R.id.appointmentEnrollFragment, false)
         } else {
             if (binding.dlMenu.isDrawerOpen(GravityCompat.START)) {
                 binding.dlMenu.closeDrawer(GravityCompat.START)
-            } else super.onBackPressed()
-            if (!bottomNavController.onBackPressed()) {
+            } else if (!bottomNavController.onBackPressed())
                 bottomNavController.onBackPressed()
-                super.onBackPressed()
-            }
+            super.onBackPressed()
         }
-
     }
-
-
 }
